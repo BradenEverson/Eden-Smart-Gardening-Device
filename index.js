e4f92b4e81bfa433b7af3a5f6774e8fda8e6ee97
Braden Everson <bradeneverson@gmail.com>
2023-05-22 11:21:02 -0500

    Added frontend

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const express = require("express");
 const fs = require("fs");
 const crypto = require("crypto");
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(__dirname + '/web'));
 app.use(cors());
 
 app.get("/", (req, res) =>{
-    res.send("Plants :)")
+    res.sendFile("web/index.html");
+});
+
+app.get("/editDevice", (req, res) =>{
+    console.log(req.params.ID);
+    res.sendFile(__dirname + "/web/edit.html");
+});
+
+app.post("/editDevice", (req, res) =>{
+    console.log(req.params.plant);
+    res.sendFile(__dirname + "/web/index.html");
 });
 
 app.get("/api/plants/waterTime/:ID", (req, res) => {
